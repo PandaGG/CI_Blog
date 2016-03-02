e05816f9540768e71b65fc01494ddc9e54ed3dc2
PandaGG <josh_zhou@hotmail.com>
2016-03-03 00:56:20 +0800

    登录页面样式

--- admin/views/home.php
+++ admin/views/home.php
@@ -7,7 +7,18 @@
 		<a href="javascript:void(0);"><i class="fa fa-plus"></i> 添加栏目 </a>
 	</div>
 	<div class="dashboard-section">
-		Home Page
+		<div class="">
+			<dl>
+				<dt>环境配置信息</dt>
+				<dd>PHP版本：<?php echo PHP_VERSION;?></dd>
+				<dd>Mysql类型：<?php echo $this->db->platform();?> /<?php echo $this->db->version();?></dd>
+				<dd>服务器端信息：<?PHP echo $_SERVER['SERVER_SOFTWARE']; ?></dd>
+				<dd>服务器操作系统： <?PHP echo PHP_OS; ?></dd>
+				<dd>运行环境：<?php echo $_SERVER['SERVER_SOFTWARE'];?></dd>
+				<dd>上传限制：<?php $max_upload = ini_get("file_uploads") ? ini_get("upload_max_filesize") : "Disabled"; echo $max_upload;?></dd>
+				<dd>服务器时间：<?php echo date("Y-m-d H:i:s",time());?></dd>
+			</dl>
+		</div>
 	</div>
 </div>
 <!-- main End -->
--- admin/views/login.php
+++ admin/views/login.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>后台登录</title>
+<title>内容管理系统</title>
+<link rel="stylesheet" href="<?php echo site_url();?>../assets/css/bootstrap.css">
+<link rel="stylesheet" href="<?php echo site_url();?>../assets/css/font-awesome.css">
+<link rel="stylesheet" href="<?php echo site_url();?>../assets/css/login.css">
 </head>
 <body>
-	<form method="post" action="login/check">
-	<div><label>Username:</label><input type="text" name="username" value="" /></div>
-	<div><label>Password:</label><input type="password" name="password" value="" /></div>
-	<div><input type="submit" value="Login" /></div>
-	</form>
+	<div class="login-body">
+		<form method="post" action="login/check">
+			<div class="login-form">
+				<h1 class="login-title">内容管理系统</h1>
+				<div class="login-box">
+					<div class="form-group">
+						<input type="text" class="form-control login-element" name="username" placeholder="Enter your user name" value="" />
+						<i class="fa fa-user"></i>
+					</div>
+					<div class="form-group">
+						<input type="password" class="form-control login-element" name="password" placeholder="Password"  value="" />
+						<i class="fa fa-lock"></i>
+					</div>
+					<div><button type="submit" class="btn btn-default btn-block login-element">Login</button></div>
+				</div>
+			</div>
+		</form>
+	</div>
 </body>
 </html>
